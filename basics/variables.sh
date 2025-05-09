f0b50a94b86a130afd2c4339a98a36b868dd02ef
Yashwant Bhosale <yashwantbhosale07@gmail.com>
2025-05-09 19:49:06 +0530

    practice program: variables in bash scripts

--- basics/variables.sh
+++ basics/variables.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 
+# Variable names
+# name of a variable can contain only:
+# letters: a to z or A to Z
+# numbers: 0-9
+# underscore: _
+
+# By convention, unix shell variables will have their name in UPPERCASE
 MESSAGE="Hello world!"
 echo $MESSAGE
+
+# Read-Only variables
+# shell provides a way to mark the variables as read-only
+NAME="yashwant"
+readonly NAME
+# now, new value cannot be assigned to NAME.
+
 
 # Program to swap two numbers without using third variable
 echo "Enter two numbers: "
